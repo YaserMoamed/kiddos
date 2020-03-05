40825378e51e7f20550db578579385481259cad5
YaserMoamd <yaser_mohamed@outlook.sa>
2020-03-05 14:39:29 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import Home from './components/Home/Home'
 const App = () => (
   <div className="app">
-    <h1>Hello world</h1>
+     <Home />
   </div>
 );
 
---
+++ src/components/contents/Corsi.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+
+const Corsi = () => {
+  return <div>Corsi</div>
+}
+export default Corsi;
