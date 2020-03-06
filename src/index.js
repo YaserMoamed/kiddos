1b26fe477d6d9b5469d8597ae4b749fbd8882856
Francesca <francescabrondi@gmail.com>
2020-03-06 14:48:14 +0100

    finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./style.css";
 import ScrollToTop from "./components/utils/ScrollToTop";
 import App from "./App";
 
