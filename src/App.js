623e783d4a3269ba8054d264cc4e102a49a838f4
emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>
2020-03-06 03:24:50 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import Home from './components/Home/Home'
-const App = () => (
+import Corsi from './components/Corsi'
+import Home from './components/Home'
+import Chisiamo from './components/Chisiamo'
+import "./components/Assets/styles.scss"
+
+import {
+  BrowserRouter as Router, Route, Link, Switch
+} from "react-router-dom"; const App = () => (
   <div className="app">
-     <Home />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/corsi" component={Corsi} />
+      <Route path="/chisiamo" component={Chisiamo} />
+    </Switch>
   </div>
 );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Navbar from './Navbar'
+
 
 const Home = () => (
   <div className="App">
-   <Navbar />
   </div>
 );
 
